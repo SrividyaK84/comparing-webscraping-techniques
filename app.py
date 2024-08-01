227dc461cc51f709e6b2c316bf216c73e3c48f0c
SrividyaK84 <87136411+SrividyaK84@users.noreply.github.com>
2024-08-01 16:07:58 +0530

    Update app.py
    gave multiselect option to the libraries instead of selecting only one at a time

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ from main import scrape
 st.title('Comparing different webscraping techniques')
 
 url = st.text_input('Enter URL:')
-library = st.selectbox('Choose scraping library:', ['jina', 'firecrawl', 'beautifulsoup', 'newspaper3k'])
+libraries = ['jina', 'firecrawl', 'beautifulsoup', 'newspaper3k']
+#library = st.selectbox('Choose scraping library:', libraries)
+
+selected_libraries = st.multiselect('Choose scraping libraries:', libraries)
+
+if st.button('Scrape'):
+    if url and selected_libraries:
+        for library in selected_libraries:
+            result = scrape(url, library)
+            st.subheader(f'Results from {library}:')
+            st.text_area(f'Scraping Results from {library}', result)
+    else:
+        st.error('Please enter a URL and select at least one scraping library.')
 
 if st.button('Scrape'):
     if url and library:
